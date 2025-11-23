9e36a50e136bc68815ebcc2f91208b6c7577cb59
panjd123 <xm_jarden@qq.com>
2025-11-23 14:55:43 +0800

    feat(docker): add docker_release.sh script to automate image building and pushing process for multiple versions

---
+++ docker_release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IMAGE_NAME="panjd123/vllama"
+
+VERSIONS=(
+  v0.8.5  # cuda12.4
+  v0.11.0 # cuda12.8
+  latest  # cuda12.9
+)
+# ===========================
+
+
+set -e
+
+for VERSION in "${VERSIONS[@]}"; do
+    echo "==============================="
+    echo "🚀 Building $IMAGE_NAME:$VERSION"
+    echo "==============================="
+
+    docker build \
+        --build-arg BASE_TAG="$VERSION" \
+        --network host \
+        -t "$IMAGE_NAME:$VERSION" .
+
+    echo "📤 Pushing $IMAGE_NAME:$VERSION"
+    docker push "$IMAGE_NAME:$VERSION"
+done
+
+echo "🎉 All images built & pushed successfully!"
